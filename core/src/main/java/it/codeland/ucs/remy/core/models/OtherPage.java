a07af3ba9c46e94054424eec4ba84381869f7c31
jhrem <jeanremy.hirwa@codeland.it>
2023-03-13 07:37:38 +0200

    hero banner start

---
+++ core/src/main/java/it/codeland/ucs/remy/core/models/OtherPage.java
@@ -0,0 +1,46 @@
+package it.codeland.ucs.remy.core.models;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+import java.util.Optional;
+
+@Model(adaptables = Resource.class)
+public class OtherPage {
+
+    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
+    @Default(values="No resourceType")
+    protected String resourceType;
+
+    @SlingObject
+    private Resource currentResource;
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+   
+
+    @PostConstruct
+    protected void init() {
+
+    }
+
+    public Page getHomePage(){
+        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+        Resource homeResource = resourceResolver.getResource("/content/academy-ucs-remy/us/en/home-page");
+        Page homePage = pageManager.getContainingPage(homeResource);
+        return homePage;
+    }   
+}
